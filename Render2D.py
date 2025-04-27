- issue with pareto front calculation fixed and made more efficient  - included null empty solution as an additional starting point for the first generation in an attempt to improve solution diversity  - fixed select_diverse_... function is now fixed and uses solution sparsity rather than looking for extreme objectives  - added a function that looks at individual routes within a solution an optimises the path taken without changing which nodes are visited keeping the demand outcome the same while improving the distance  - added points key to graph legend  - fixed hypervolume calculation  - added a prompt for running an initial number of generations automatically  - added generation count and addition instructions

Render2D.py
@@ -83,13 +83,13 @@ class GraphObj:
         self.graph_width = self.p3[0] - self.p1[0]
         self.graph_height = self.p3[1] - self.p1[1]
 
-        self.label_surface = pg.Surface((self.size[0]/2.5, self.size[1]/6))
+        self.label_surface = pg.Surface((self.size[0]/2.5, self.size[1]/3))
 
         self.hypervolume_indicator = 0
 
     def sort_pareto_front(self):
         pareto_distances = [self.data[0][i] for i in self.pareto_front]
-        sorted_pareto_indices = np.argsort(pareto_distances)
+        sorted_pareto_indices = np.flip(np.argsort(pareto_distances))       
         self.sorted_pareto_front = [self.pareto_front[i] for i in sorted_pareto_indices]
 
     def update(self, new_data, pareto_front):
@@ -108,15 +108,29 @@ class GraphObj:
 
         self.calculate_hypervolume()
 
-        label_heading = self.font.render("Metrics:", True, (0,200,0))
+        label_heading = self.font.render("Key:", True, (0,200,0))
         label_H_Indicator1 = self.font.render("Normalised Hypervolume ", True, (0,200,0))
         label_H_Indicator2 = self.font.render(f"Indicator = {self.hypervolume_indicator:.4f}", True, (0,200,0))
+
+        label_red_key = self.font.render(" = Historical Solution", True, (0, 200, 0))
+        label_yellow_key = self.font.render(" = Just Added", True, (0, 200, 0))
+        label_cyan_key = self.font.render(" = Next Generation", True, (0, 200, 0))
+        label_blue_key = self.font.render(" = Pareto Front", True, (0, 200, 0))
 
         self.label_surface.fill((10, 20, 10))
         pg.draw.rect(self.label_surface, (0, 200, 0), (0, 0, self.label_surface.get_width(), self.label_surface.get_height()), width=2)
         self.label_surface.blit(label_heading, (5,5))
-        self.label_surface.blit(label_H_Indicator1, (5,30))
-        self.label_surface.blit(label_H_Indicator2, (5,45))
+        self.label_surface.blit(label_red_key, (10,30))
+        pg.draw.circle(self.label_surface, (255, 0, 0), (10, 37), 3)
+        self.label_surface.blit(label_yellow_key, (10,50))
+        pg.draw.circle(self.label_surface, (255, 255, 0), (10, 57), 3)
+        self.label_surface.blit(label_cyan_key, (10,70))
+        pg.draw.circle(self.label_surface, (0, 200, 250), (10, 77), 3)
+        self.label_surface.blit(label_blue_key, (10,90))
+        pg.draw.circle(self.label_surface, (0, 0, 255), (10, 97), 2)
+
+        self.label_surface.blit(label_H_Indicator1, (5, 125))
+        self.label_surface.blit(label_H_Indicator2, (5, 140))
 
     def draw_surface(self):
         self.surface.fill((0,0,0))
@@ -153,12 +167,12 @@ class GraphObj:
             for i, x, y in zip(list(range(len(self.data[0]))), self.data[0], self.data[1]):                
                 if i in self.highlight_population:
                     plot_x, plot_y = self.plot_xy(x,y)
-                    pg.draw.circle(self.surface, (0, 200, 250), (int(plot_x), int(plot_y)), 6)
+                    pg.draw.circle(self.surface, (0, 200, 250), (int(plot_x), int(plot_y)), 3)
 
             for i, x, y in zip(list(range(len(self.data[0]))), self.data[0], self.data[1]):
                 if i in self.pareto_front:
                     plot_x, plot_y = self.plot_xy(x,y)
-                    pg.draw.circle(self.surface, (0, 0, 255), (int(plot_x), int(plot_y)), 3)
+                    pg.draw.circle(self.surface, (0, 0, 255), (int(plot_x), int(plot_y)), 2)
 
         self.surface.blit(self.label_surface, (self.size[0] - self.label_surface.get_width(), 0))
 
@@ -173,12 +187,14 @@ class GraphObj:
         label_heading = self.font.render("Selected Solution:", True, (0, 200, 0))
         label_demand = self.font.render(f"Demand = {y:.2f}", True, (0, 200, 0))
         label_distance = self.font.render(f"Distance = {x:.2f}", True, (0, 200, 0))
+        label_id = self.font.render(f"Solution number = {self.sorted_pareto_front[selected_solution]}", True, (0,200,0))
 
         self.label_surface.fill((10, 20, 10))
         pg.draw.rect(self.label_surface, (0, 200, 0), (0, 0, self.label_surface.get_width(), self.label_surface.get_height()), width=2)
         self.label_surface.blit(label_heading, (5,5))
         self.label_surface.blit(label_demand, (5,30))
-        self.label_surface.blit(label_distance, (5,55))    
+        self.label_surface.blit(label_distance, (5,55))
+        self.label_surface.blit(label_id, (5, 80))
 
     def plot_xy(self, x, y):
         plot_x = self.p2[0] + (x-self.minX) * self.graph_width / self.rangeX
@@ -187,28 +203,34 @@ class GraphObj:
         return plot_x, plot_y
 
     def calculate_hypervolume(self):
-        ref_distance = max(self.data[0])
-        ref_demand = max(self.data[1])
-        ref_point = np.array((ref_distance, ref_demand))
+        min_dist = min(self.data[0])
+        max_dist = max(self.data[0])
+        min_demand = min(self.data[1])
+        max_demand = max(self.data[1])
+        
+        ref_distance = max_dist * 1.1 #reference point is worse than the worst objectives
+        ref_demand = max_demand * 1.1
 
-        self.hypervolume_indicator = 0
+        self.hypervolume_indicator = 0.0
+        prev_dist = ref_distance
 
-        for i, solution_number in enumerate(self.sorted_pareto_front):
+
+        for solution_number in self.sorted_pareto_front:
             dist = self.data[0][solution_number]
             dmnd = self.data[1][solution_number]
 
-            if i==0:
-                width = dist - ref_point[0]
-            else:
-                width = dist - self.data[0][self.sorted_pareto_front[i-1]]
+            width = prev_dist - dist 
 
-            height = ref_point[1] - dmnd
+            height = ref_demand - dmnd
 
             self.hypervolume_indicator += width*height
+            prev_dist = dist
 
         # normalise the hypervolume indicator:
-        maximum_hypervolume = (ref_distance - min(self.data[0])) * (ref_demand - min(self.data[1]))
-        if maximum_hypervolume > 0:
+        maximum_hypervolume = (ref_distance - min_dist) * (ref_demand - min_demand)
+        if maximum_hypervolume <= 0:
+            self.hypervolume_indicator = 0.0
+        else:
             self.hypervolume_indicator /= maximum_hypervolume
 
 

Travelling_Salesman.py
@@ -8,6 +8,12 @@ import optimiser as op
 import Render2D as r
 
 
+try:
+    starting_gens = int(input("Enter the number of generations to start with (e.g: 10):"))
+except:
+    print("invalid input. Starting with a single generation")
+    starting_gens = 1
+
 pg.init()
 clock = pg.time.Clock()
 
@@ -28,7 +34,7 @@ with open("data//vrp8.txt", "r") as f:
         initial_connections.append(int(n)-2)
 points = np.array(points)
 
-optimiser = op.MOGA_PTSP(points, demands, 10)
+optimiser = op.MOGA_PTSP(points, demands, 30)
 graph = r.GraphObj(np.array([0,0]), np.array(op.extract_objectives(optimiser.solution_history)), np.array([500,500]), "MOGA", "total distance", "remaining demand")
 
 camera = r.Camera2D((width, height))
@@ -50,6 +56,7 @@ running = True
 speed = 8
 select = False
 selected_solution = 0
+optimise_selected = False
 
 horr = 0
 vert = 0
@@ -66,16 +73,22 @@ for s, mapsOfSolution in enumerate(mapsOfPopulation):
         routeMap.update(optimiser.solution_history[optimiser.population[s]].solution[r])
 
 instruction_font = pg.font.SysFont("Consolas", 20)
-instruction1 = instruction_font.render("Press space to create the next generation", True, (255,0,0))
-instruction2 = instruction_font.render("Use the arrow keys to scroll around the screen", True, (255, 0,0))
-instruction3 = instruction_font.render("Press S to enter selection mode", True, (255,0,0))
-instruction4 = instruction_font.render("Use arrow keys to choose a solution on the pareto front", True, (255,0,0))
-instruction5 = instruction_font.render("Press S to exit selection mode", True, (255,0,0))
+instruction1 = instruction_font.render("Press space to create the next generation", True, (255, 0 ,0))
+instruction2 = instruction_font.render("Use the arrow keys to scroll around the screen", True, (255, 0, 0))
+instruction2_5 = instruction_font.render("Press enter to run pareto distance optimisation", True, (255, 0, 0))
+instruction3 = instruction_font.render("Press S to enter selection mode", True, (255, 0, 0))
+instruction4 = instruction_font.render("Use arrow keys to choose a solution on the pareto front", True, (255, 0, 0))
+instruction4_5 = instruction_font.render("Press enter to optimise the distance of selected solution", True, (255, 0, 0))
+instruction5 = instruction_font.render("Press S to exit selection mode", True, (255,0 , 0))
+instruction6 = instruction_font.render("Generation: 0", True, (255,0,0))
+
+gen = 0
 
 dt = 0
 while running:
     space = False
     select_arrows = 0
+    optimise_selected = False
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
@@ -107,14 +120,7 @@ while running:
                 select = not select
 
             elif event.key == pg.K_RETURN:
-                # debug:
-
-                for a, solution_a in enumerate(optimiser.solution_history):
-                    for b, solution_b in enumerate(optimiser.solution_history):
-                        if solution_a.solution == solution_b.solution:
-                            if a!=b:
-                                if solution_b.demand != solution_a.demand:
-                                    print(solution_b.demand, solution_a.demand)
+                optimise_selected = True
 
 
 
@@ -122,8 +128,10 @@ while running:
     if not select:
         camera.position -= arrow_vector
 
-    if arrow_vector[0] == 0 and arrow_vector[1] == 0 and space and not select:
-        screen.blit(loading_text, (width - loading_text.get_width(), 70))
+    if arrow_vector[0] == 0 and arrow_vector[1] == 0 and (space or starting_gens > 0) and not select:
+        screen.blit(loading_text, (width - loading_text.get_width(), 120))
+        gen +=1
+        instruction6 = instruction_font.render(f"Generation: {gen}", True, (255,0,0))
         pg.display.flip()
         screen.fill((0, 0, 0))
         optimiser.genetic_optimiser()
@@ -137,13 +145,27 @@ while running:
                 routeMap.colour = np.array((255, 0, 0))
                 routeMap.update(optimiser.solution_history[optimiser.population[s]].solution[r])
 
+        if starting_gens > 0:
+            starting_gens += -1
+
     if select and len(graph.pareto_front) > 0:
+
+        if optimise_selected:
+            gen+=1
+            instruction6 = instruction_font.render(f"Generation: {gen}", True, (255,0,0))
+            optimiser.optimise_distance_of_selected_solution(graph.sorted_pareto_front[selected_solution])
+
+            graph_data = np.array(op.extract_objectives(optimiser.solution_history))
+            graph.update(graph_data, optimiser.pareto_front)
+
         if select_arrows != 0:
             selected_solution += select_arrows
         if selected_solution < 0:
             selected_solution = len(graph.pareto_front)-1
         if selected_solution >= len(graph.pareto_front):
             selected_solution = 0
+
+
         
         graph.draw_surface()
         graph.select_solution(selected_solution)
@@ -153,6 +175,16 @@ while running:
             routeMap.update(optimiser.solution_history[graph.sorted_pareto_front[selected_solution]].solution[r])
 
     else:
+        if optimise_selected:
+            screen.blit(loading_text, (width - loading_text.get_width(), 120))
+            pg.display.flip()
+            for i in optimiser.pareto_front:
+                optimiser.optimise_distance_of_selected_solution(i)
+            
+            optimiser.pareto_front = op.findParetoFront(optimiser.solution_history)
+            optimiser.population = optimiser.pareto_front.copy()
+            graph_data = np.array(op.extract_objectives(optimiser.solution_history))
+            graph.update(graph_data, optimiser.pareto_front)
         graph.draw_surface()                
 
     camera.render()
@@ -161,10 +193,13 @@ while running:
     if not select:
         screen.blit(instruction1, (width-instruction1.get_width(), 0))
         screen.blit(instruction2, (width-instruction2.get_width(), 25))
-        screen.blit(instruction3, (width-instruction3.get_width(), 50))
+        screen.blit(instruction2_5, (width-instruction2_5.get_width(), 50))
+        screen.blit(instruction3, (width-instruction3.get_width(), 75))
+        screen.blit(instruction6, (width-instruction6.get_width(), 100))
     else:
         screen.blit(instruction4, (width-instruction4.get_width(), 0))
-        screen.blit(instruction5, (width-instruction5.get_width(), 25))
+        screen.blit(instruction4_5, (width-instruction4_5.get_width(), 25))
+        screen.blit(instruction5, (width-instruction5.get_width(), 50))
 
     pg.display.flip()
     dt = clock.tick(30) / 1000

optimiser.py
@@ -3,16 +3,17 @@ import numpy as np
 rng = np.random.default_rng()
 
 def findParetoFront(solution_history):
-    pareto_front = []
     distances, demands = extract_objectives(solution_history)
-    for i in range(len(solution_history)):
-        i_is_better_than_these_di = distances[i] < distances
-        i_is_better_than_these_de = demands[i] < demands
-
-        bool_list = np.logical_or(i_is_better_than_these_de, i_is_better_than_these_di)
-        if len(bool_list)-sum(bool_list) <= 1 :
-            pareto_front.append(i)
-
+    num = len(distances)
+    dominated = np.zeros(num, dtype=bool)
+    
+    for i in range(num):
+        # Compare i against all j
+        dominated_by_i = (distances <= distances[i]) & (demands <= demands[i]) 
+        dominated_by_i &= ((distances < distances[i]) | (demands < demands[i]))
+        dominated[i] = np.any(dominated_by_i)
+    
+    pareto_front = np.where(np.bitwise_not(dominated))[0].tolist()
     return pareto_front
 
 def extract_objectives(solution_history):
@@ -26,6 +27,8 @@ def extract_objectives(solution_history):
     return distances, demands
 
 def normalise(value, min_val, max_val):
+    if max_val == min_val:
+        return 0.5
     return (value - min_val) / (max_val - min_val)
 
 def findBestSolutions(solution_indexes, number_of_solutions, solution_history):
@@ -46,12 +49,13 @@ def findBestSolutions(solution_indexes, number_of_solutions, solution_history):
 
     dominated_by = np.array(dominated_by)
 
+
     let_through = []
     threshold = 0
     while len(let_through) < number_of_solutions:
         for c, i in enumerate(dominated_by):
             if i == threshold:
-                let_through.append(solution_indexes[i])
+                let_through.append(solution_indexes[c])
             if len(let_through) == number_of_solutions:
                 break
         threshold += 1
@@ -78,11 +82,16 @@ class MOGA_PTSP:
         # each route consists of a series of indexes referring to the self.points list indicating the movement from one node to the next
         self.population = []
         self.solution_history = []
-        for i in range(self.population_size):
+        for i in range(self.population_size-1):
             solution = [self.generate_random_route() for j in range(self.periods)]
             self.solution_history.append(Solution(solution, self.all_distances, self.starting_demands))
             self.solution_history[-1].evaluate_objectives(self.all_distances)
             self.population.append(i)
+
+        # add in the null solution of empty routes:
+        null_solution = [[self.depot] for i in range(self.periods)]
+        self.solution_history.append(Solution(null_solution, self.all_distances, self.starting_demands))
+        self.population.append(len(self.solution_history) - 1)
 
         self.pareto_front = []
 
@@ -145,7 +154,7 @@ class MOGA_PTSP:
         
         for s, solution_index in enumerate(self.population):
             # determine removal probability by the solution in the population:
-            prob = 0.3
+            prob = s / len(self.population)
             original = self.solution_history[solution_index].solution
             altered_solution = [route.copy() for route in original]
             for r, route in enumerate(original):
@@ -168,7 +177,7 @@ class MOGA_PTSP:
 
         new_populations = []
         for i in range(1, 10):
-            new_populations.append(self.mutate(3))
+            new_populations.append(self.mutate(5))
 
         # combine the new with the old
         for population in new_populations:
@@ -180,11 +189,7 @@ class MOGA_PTSP:
 
         if len(self.population) < self.population_size:
             number_of_solutions = self.population_size - len(self.population)
-            if number_of_solutions % 2 != 0:
-                number_of_solutions+=1
-            self.select_diverse_solutions_from_history("distance", int(number_of_solutions/2))
-            self.select_diverse_solutions_from_history("demand", int(number_of_solutions/2))
-
+            self.select_diverse_solutions_from_history(number_of_solutions)
 
         # eliminate the worst solutions:
         self.population = findBestSolutions(self.population, self.population_size, self.solution_history)
@@ -223,42 +228,83 @@ class MOGA_PTSP:
                 remaining.append(self.population[i])
         self.population = remaining
 
-    def select_diverse_solutions_from_history(self, objective, num_sols):
+    def select_diverse_solutions_from_history(self, num_sols, computation_cutoff=200):
         distances, demands = extract_objectives(self.solution_history)
         
-        if objective == "distance":
-            weighting = 1 / np.array(distances)
-        elif objective == "demand":
-            weighting = 1 / np.array(demands)
-        else:
-            raise ValueError("Invalid objective. Choose distance or demand")
+        effective_cutoff = min(len(self.solution_history), computation_cutoff)
+        distances = distances[-effective_cutoff:]
+        demands = demands[-effective_cutoff:]
 
-        weighting = (weighting / np.sum(weighting)).tolist()
-        
-        to_append = np.random.choice(list(range(len(self.solution_history))), num_sols, replace=False, p=weighting)
+        distances = normalise(np.array(distances), np.min(distances), np.max(distances)).tolist()
+        demands = normalise(np.array(demands), np.min(demands), np.max(demands)).tolist()
+  
+        solution_sparsity = []
+        for dist1, dmnd1 in zip(distances, demands):
+            objective_summed_distance = np.array([0, 0], dtype=np.float64)
+            for dist2, dmnd2 in zip(distances, demands):
+                if not(dist1==dist2 and dmnd1==dmnd2):
+                    objective_summed_distance += np.array([abs(dist1-dist2), abs(dmnd1-dmnd2)])
+            solution_sparsity.append(np.linalg.norm(objective_summed_distance))
+
+
+        weighting = (solution_sparsity / np.sum(solution_sparsity)).tolist()
+
+        offset = max(0, len(self.solution_history) - effective_cutoff) # re-indexing required
+        to_append = np.random.choice(range(effective_cutoff), num_sols, replace=False, p=weighting)
         for i in to_append:
-            self.population.append(i)  
-    
+            self.population.append(i+offset) # use this to ensure that index is consistent with solution history
+
+        
+    def dist_of_route(self, route):
+        summation = 0
+        for i in range(len(route)):
+            summation += self.all_distances[route[i]][route[i-1]]
+        return summation
+
+    def optimise_distance_of_selected_solution(self, selected_solution, dont_update=False):
+        solution = [route.copy() for route in  self.solution_history[selected_solution].solution] 
+
+        for route in solution:
+            if len(route) >= 2:
+                for iteration in range(50):
+
+                    a = np.random.randint(1, len(route))
+                    b = np.random.randint(1, len(route))
+                    if a!=b:
+                        original_dist = self.dist_of_route(route)
+                        route[a], route[b] = route[b], route[a]
+                        new_dist = self.dist_of_route(route)
+
+                        if new_dist > original_dist:
+                            route[a], route[b] = route[b], route[a]
+
+        self.solution_history.append(Solution(solution, self.all_distances, self.starting_demands))
+        #new population is all the new found solution which will all get mutated when there is another generation
+        self.population = [len(self.solution_history)-1 for i in range(self.population_size)]
+
+        if not dont_update:
+            self.pareto_front = findParetoFront(self.solution_history)
+                
 
 # a class which keeps track of each solution and their objective values
 class Solution:
     def __init__(self, solution, distance_array, demands):
         self.solution = [route.copy() for route in solution]      # deep copy routes
-        self.demands  = demands.copy()  
+        self.initial_demands  = demands.copy()  
         self.first_solution = solution.copy()
         self.demands = demands.copy()
         self.distance, self.demand = self.evaluate_objectives(distance_array)
 
         
     def evaluate_objectives(self, all_distances):
-
+        demands = self.initial_demands.copy()
         total_distance = 0
         for route in self.solution:
             for p, point in enumerate(route):
                 prev_point = route[p-1] if p > 0 else route[-1]
                 total_distance += all_distances[prev_point][point]
-                if self.demands[point] > 0:# if there is no demand in a visited node, then there is no reduction in the total demand
-                    self.demands[point] -= 1
+                if demands[point] > 0:# if there is no demand in a visited node, then there is no reduction in the total demand
+                    demands[point] -= 1
 
-        return total_distance, sum(self.demands)
+        return total_distance, sum(demands)
 